b86f11eaf56d612a68dbc2b8042e16b520fedea4
Marcel Moreaux <git@moreaux.nl>
2023-03-27 22:13:27 +0200

    Updated NotePad style

--- style/NotePad/generate-font-variants.sh
+++ style/NotePad/generate-font-variants.sh
@@ -4,9 +4,6 @@ FONT="ArchitectsDaughter"
 BOLD=40
 SKEW=0.5
 
-fontforge -c "f = open('$FONT-Regular.ttf'); f.selection.all(); f.changeWeight($BOLD).generate('$FONT-Bold.ttf')"
-fontforge -c "f = open('$FONT-Regular.ttf'); f.selection.all(); f.transform(psMat.skew($SKEW)).transform(psMat.scale(1, 0.9)).generate('$FONT-Italic.ttf')"
-fontforge -c "f = open('$FONT-Regular.ttf'); f.selection.all(); f.transform(psMat.skew($SKEW)).transform(psMat.scale(1, 0.9)).changeWeight($BOLD).generate('$FONT-BoldItalic.ttf')"
-
-#FONT="MarckScript"
-#fontforge -c "f = open('$FONT-Regular.ttf'); f.selection.all(); f.changeWeight($BOLD).generate('$FONT-Bold.ttf')"
+fontforge -c "f = open('$FONT-Regular.ttf'); f.selection.all(); f.fontname = f.fontname.replace('Regular', 'Bold'); f.fullname = f.fullname.replace('Regular', 'Bold'); f.weight = 'Bold'; f.changeWeight($BOLD).generate('$FONT-Bold.ttf')"
+fontforge -c "f = open('$FONT-Regular.ttf'); f.selection.all(); f.fontname = f.fontname.replace('Regular', 'Italic'); f.fullname = f.fullname.replace('Regular', 'Italic'); f.transform(psMat.skew($SKEW)).transform(psMat.scale(1, 0.9)).generate('$FONT-Italic.ttf')"
+fontforge -c "f = open('$FONT-Regular.ttf'); f.selection.all(); f.fontname = f.fontname.replace('Regular', 'BoldItalic'); f.fullname = f.fullname.replace('Regular', 'BoldItalic'); f.weight = 'Bold'; f.transform(psMat.skew($SKEW)).transform(psMat.scale(1, 0.9)).changeWeight($BOLD).generate('$FONT-BoldItalic.ttf')"
